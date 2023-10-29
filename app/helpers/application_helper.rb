9ac01a36b441a1909774873f7aed061a9109818a
waterlow <waterlow3442@gmail.com>
2023-10-29 17:19:03 +0900

    add full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def full_title(page_title)
+    base_title = 'Ruby on Rails Tutorial Sample App'
+    if page_title.present?
+      "#{page_title} | #{base_title}"
+    else
+      base_title
+    end
+  end
 end
