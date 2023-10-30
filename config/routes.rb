f3ddbcbb802d6b9cc4f4af514ece861a11a91e1b
waterlow <waterlow3442@gmail.com>
2023-10-30 09:26:44 +0900

    add signup page

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get '/about', to: 'static_pages#about'
   get '/help', to: 'static_pages#help'
   get '/contact', to: 'static_pages#contact'
+  get '/signup',  to: 'users#new'
   resources :products
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
